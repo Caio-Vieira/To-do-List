f3d0b64d07007a812c06fb8f0241e408e4c6f476
Caio Vieira <caio.vieira1910@gmail.com>
2023-05-25 01:40:19 -0300

    refatorou e corrigiu o código

--- README.md
+++ README.md
@@ -1,7 +1,1 @@
 #  📝 Lista de Tarefas 📝
-
-![Captura de Tela 2021-03-19 às 13 59 47](https://user-images.githubusercontent.com/62302606/111816701-e52b1580-88bb-11eb-8450-491a5a5c6fd8.png)
-
-Lista de Tarefas criada com HTML, CSS e JavaScript
-
-Página: https://caio-vieira.github.io/To-do-List/
--- script.js
+++ script.js
@@ -1,68 +1,68 @@
-let inputText = document.getElementById('text')
-let btn = document.getElementById('send')
+let textInput = document.getElementById('text')
+let sendButton = document.getElementById('send')
 let container = document.getElementById('container')
-let listTask = []
+let todoList = []
 
-btn.addEventListener('click', addTask)
+sendButton.addEventListener('click', addTask)
 
-function randomId() {
+function generateID() {
     return Math.floor(Math.random() * 1000)
 }
 
-function textTask() {
-    return { task: inputText.value, id: randomId() }
+function taskContent() {
+    return { id: generateID(), task: textInput.value }
 }
 
 function addTask() {
-    if (inputText.value == '') {
-        alert('digite uma nota')
-        inputText.focus()
+    if (textInput.value == '') {
+        textInput.focus()
+        return alert('digite uma nota')
     }
-    else {
-        listTask.push(textTask())
-        inputText.value = ''
-        inputText.focus()
-        showTask(listTask)
-        localStorage.setItem("lista", JSON.stringify(listTask))
-    }
+
+    todoList.push(taskContent())
+    textInput.value = ''
+    textInput.focus()
+    showTasks(todoList)
+    localStorage.setItem("lista", JSON.stringify(todoList))
+
 }
 
-function showTask(task) {
+function showTasks(task) {
+    let element = ''
 
-    task.forEach(item => {
-
-        text = "<div id=" + item.id + " class=tasks" + ">" + "<input type='radio' name='selected'>" + "<p>" + item.task + "</p>" + "</div>"
+    task.forEach((item, index) => {
+        element = createElementHTML(item, index)
     })
 
-    container.innerHTML += text
+    container.innerHTML += element
 }
 
 function removeItem() {
-    let inputChecked = document.querySelectorAll('input[type=radio]')
+    let checkInput = document.querySelectorAll('input[type=radio]')
 
-    inputChecked.forEach((item) => {
+    checkInput.forEach((item) => {
         if (item.checked) {
             container.removeChild(item.parentElement)
-            let takeId = parseInt(item.parentElement.getAttribute('id'))
-            let deleteItem = listTask.map((item) => item.id).indexOf(takeId)
-            listTask.splice(deleteItem, 1)
-            localStorage.setItem("lista", JSON.stringify(listTask))
+            let getId = parseInt(item.parentElement.getAttribute('id'))
+            let deleteItem = todoList.findIndex((item) => item.id == getId)
+            todoList.splice(deleteItem, 1)
+            localStorage.setItem("lista", JSON.stringify(todoList))
         }
     })
 }
 
 function removeAll() {
     if (container.childElementCount == 0) {
-        alert('Insira uma nota')
-        inputText.focus()
+        textInput.focus()
+        return alert('Insira uma nota')
     }
     else {
         let confirme = confirm('Tem certeza que gostaria de apagar todas as notas?')
-        if (confirme == true) {
+        if (confirme) {
             container.innerHTML = ''
-            listTask = []
-            inputText.value = ''
-            inputText.focus()
+            todoList = []
+            textInput.value = ''
+            textInput.focus()
             localStorage.clear('lista')
         }
     }
@@ -71,15 +71,21 @@ function removeAll() {
 function loadTasks() {
 
     let tasksLoad = JSON.parse(localStorage.getItem('lista'))
+    let element = ''
 
     if (tasksLoad == null) {
         addTask
     }
     else {
-        listTask = tasksLoad
-        listTask.forEach(item => {
-            text = "<div id=" + item.id + " class=tasks" + ">" + "<input type='radio' name='selected'>" + "<p>" + item.task + "</p>" + "</div>"
-            container.innerHTML += text
+        todoList = tasksLoad
+        todoList.forEach((item, index) => {
+            element = createElementHTML(item, index)
+            container.innerHTML += element
         })
     }
+}
+
+
+function createElementHTML(item, index) {
+    return `<div id="${item.id}" class="tasks"><input type="radio" name="selected${index}"><p>${item.task}</p></div>`
 }
